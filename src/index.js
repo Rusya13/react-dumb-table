8ef1d48ee7cb036fd7d7793aa39da17eedf6c9e9
rusya13@gmail.com <rusya13@gmail.com>
2017-02-14 00:33:57 +0100

    selected limit reload

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class SimpleDataTable extends React.Component {
 
     componentDidMount() {
         window.addEventListener( "resize", this.resizeTable.bind( this ) )
-        this._setColumnsSize(this.props.columns)
+        this._setColumnsSize( this.props.columns )
     }
 
     componentWillUnmount() {
@@ -59,7 +59,7 @@ class SimpleDataTable extends React.Component {
 
             columns[ index ].width += newDiff;
             columns[ index + 1 ].width -= newDiff;
-            this._setColumnsSize(this.props.columns);
+            this._setColumnsSize( this.props.columns );
             originalWidth  = newSize;
             originalOffset = newOffset;
 
@@ -70,21 +70,21 @@ class SimpleDataTable extends React.Component {
         }.bind( this );
     }
 
-    _setColumnsSize(columns){
+    _setColumnsSize( columns ) {
 
         //style={col.width && { width: col.width + "%" || "auto" }}
-        let colgroupHeader = document.getElementsByClassName("data-table_colgroup-header") || [];
-        let colgroupBody = document.getElementsByClassName("data-table_colgroup-body") || [];
-        let colgroupBottomRow = document.getElementsByClassName("data-table_colgroup-bottom-row") || [];
+        let colgroupHeader    = document.getElementsByClassName( "data-table_colgroup-header" ) || [];
+        let colgroupBody      = document.getElementsByClassName( "data-table_colgroup-body" ) || [];
+        let colgroupBottomRow = document.getElementsByClassName( "data-table_colgroup-bottom-row" ) || [];
         //console.log("index _setColumnsSize", colgroupHeader);
         //console.log("index _setColumnsSize", colgroupBody);
         //console.log("index _setColumnsSize", colgroupBottomRow);
 
-        columns.forEach((column, index)=>{
-            colgroupHeader[index].style.width = column.width + "%";
-            colgroupBody[index].style.width = column.width + "%";
+        columns.forEach( ( column, index ) => {
+            colgroupHeader[ index ].style.width = column.width + "%";
+            colgroupBody[ index ].style.width   = column.width + "%";
             //colgroupBottomRow[index].style.width = column.width + "%";
-        })
+        } )
 
     }
 
@@ -92,24 +92,25 @@ class SimpleDataTable extends React.Component {
         return (
             <colgroup>
                 {columns.map( ( col, index ) => {
-                    return <col className={"data-table_colgroup-"+place} data-index={index} key={index} />
-                })}
+                    return <col className={"data-table_colgroup-" + place} data-index={index} key={index}/>
+                } )}
             </colgroup>
         )
     }
 
-    _renderHeader( columns ) {
+    _renderHeader( columns , headerHeight) {
         return (
             <thead>
-            <tr>
+            <tr >
                 {columns.map( ( col, index ) => {
                     return (
-                        <th className="flex-wrap" key={index}>
-                            <div className="box">{col.title}</div>
+                        <th style={{height:headerHeight}}  className="flex-wrap" key={index}>
+                            <div className="data-table__header-box">{col.title}</div>
                             <div className="flex-wrap-div" data-index={index}>
                                 <div
+                                    style={{height:headerHeight}}
                                     onMouseDown={this.handleMouseDown2.bind( this )}
-                                    style={{ cursor: "col-resize" }}
+
                                     className="resize-handle-new"/>
                             </div>
                         </th>
@@ -121,27 +122,36 @@ class SimpleDataTable extends React.Component {
     }
 
 
-    _renderRow( row, columns ){
-        return columns.map((column, cellIndex)=>{
+    _renderRow( row, columns ) {
+        return columns.map( ( column, cellIndex ) => {
 
-            let value = row[column.key];
+            let value = row[ column.key ];
             //console.log("index value", value);
             return (
-                <td key={cellIndex}>
+                <td className="data-table__content-cell" key={cellIndex}>
                     {value}
                 </td>
             )
-        })
+        } )
     }
 
-    _renderBody( data, columns ) {
+    _rowSelectHandler(row, index){
+        this.props.rowSelectHandler && this.props.rowSelectHandler(row, index)
+
+    }
+
+    _renderBody( data, columns, rowHeight, selectedRowIndex ) {
         return (
             <tbody>
             {data.map( ( row, index ) => {
-
+                let className = "data-table__content-row";
+                if (selectedRowIndex) className += "data-table__content-row-selected";
                 return (
-                    <tr key={index}>
-                        {this._renderRow(row, columns)}
+                    <tr style={{height:rowHeight}}
+                        className={className}
+                        key={index}
+                        onClick={()=>{this._rowSelectHandler(row, index)}}>
+                        {this._renderRow( row, columns )}
                     </tr>
                 )
             } )}
@@ -168,34 +178,94 @@ class SimpleDataTable extends React.Component {
         )
     }
 
+    _renderPagination(limit, offset, total){
+
+    }
+
+    _renderReloadButton(){
+        return(
+            <button className="data-table__footer-reload-button" onClick={this.props.reloadButtonHandler}>
+                <svg viewBox="0 0 24 24" width="24" height="24">
+                    <path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z">
+                    </path>
+                </svg>
+            </button>
+        )
+    }
+    _renderLimitSelector(limit, limitsList){
+        return (
+            <div className="data-table__footer-limit-selector-wrapper">
+                <select
+                    className="data-table__footer-select"
+                    onChange={(e)=>this.props.limitSelectorHandler && this.props.limitSelectorHandler(e.target.value)}>
+                    {limitsList.map(limit=>{
+                        return <option key={limit} value={limit}>{limit}</option>
+                    })}
+                </select>
+                <svg viewBox="0 0 24 24" width="24" height="24"><path d="M7 10l5 5 5-5z"></path></svg>
+            </div>
+        )
+    }
+
     render() {
-        let columns   = this.props.columns;
-        let data      = this.props.data;
-        let bottomRow = this.props.bottomRow;
+        console.log("index render");
+        let columns    = this.props.columns;
+        let data       = this.props.data;
+        let rowHeight = this.props.rowHeight;
+        let bottomRow  = this.props.bottomRow;
+        let showFooter = this.props.showFooter;
+        let footerHeight = this.props.footerHeight;
+        let first_num = 1;
+        let last_num = 10;
+        let total = this.props.total;
+        let limit = this.props.limit;
+        let offset = this.props.offset;
+        let limitsList = this.props.limitsList;
+
+        let headerHeight = this.props.headerHeight;
+        let selectedRowIndex = this.props.selectedRowIndex;
 
         return (
             <div id="table" ref={( ref ) => this.tableWrapper = ref} className="data-table">
                 <div
                     ref={( ref ) => this.header = ref}
+
                     className="data-table__header">
                     <table ref={( ref ) => this.table = ref}>
                         {this._renderColumnsSync( columns, "header" )}
-                        {this._renderHeader( columns )}
+                        {this._renderHeader( columns, headerHeight )}
                     </table>
                 </div>
                 <div className="data-table__content">
                     <table ref={( ref ) => this.tableBody = ref}>
-                        {this._renderColumnsSync( columns , "body")}
-                        {this._renderBody( data, columns )}
+                        {this._renderColumnsSync( columns, "body" )}
+                        {this._renderBody( data, columns, rowHeight, selectedRowIndex )}
                     </table>
                 </div>
                 {
                     bottomRow ?
-                        <div className="data-table_bottom_row">
+                        <div className="data-table__bottom_row">
                             <table ref={( ref ) => this.tableBottomRow = ref}>
-                                {this._renderColumnsSync( columns , "bottom-row")}
+                                {this._renderColumnsSync( columns, "bottom-row" )}
                                 {this._renderBottomRow( columns )}
                             </table>
+                        </div>
+                        :
+                        null
+                }
+                {
+                    showFooter ?
+                        <div className="data-table__footer" style={{height:footerHeight}}>
+                            <div className="data-table__footer-info">
+                                <div>Показано c {first_num} по {last_num} из {total}</div>
+                            </div>
+                            <div className="data-table__footer-pagination">
+                                {this._renderPagination(limit, offset, total)}
+                            </div>
+                            <div className="data-table__footer-settings">
+                                {this._renderReloadButton()}
+                                {this._renderLimitSelector(limit, limitsList)}
+                            </div>
                         </div>
                         :
                         null
@@ -209,43 +279,79 @@ class SimpleDataTable extends React.Component {
 }
 
 SimpleDataTable.propTypes = {
-    height:    PropTypes.number.isRequired,
-    columns:   PropTypes.array.isRequired,
-    data:      PropTypes.array.isRequired,
-    bottomRow: PropTypes.bool
+    height:     PropTypes.number.isRequired,
+    columns:    PropTypes.array.isRequired,
+    data:       PropTypes.array.isRequired,
+    rowHeight: PropTypes.number,
+    bottomRow:  PropTypes.bool,
+
+    rowSelectHandler: PropTypes.func,
+    selectedRowIndex:PropTypes.number,
+
+    showFooter: PropTypes.bool,
+    footerHeight: PropTypes.any,
+    limit:PropTypes.number,
+    offset:PropTypes.number,
+    total:PropTypes.number,
+    reloadButtonHandler:PropTypes.func,
+    limitSelectorHandler:PropTypes.func,
+    limitsList:PropTypes.arrayOf(PropTypes.number),
+
+    headerHeight:PropTypes.number
 };
 
 SimpleDataTable.defaultProps = {
-    height:    500,
-    columns:   [],
-    data:      [],
-    bottomRow: false
+    height:     500,
+    columns:    [],
+    data:       [],
+    rowHeight: 30,
+    bottomRow:  false,
+
+    rowSelectHandler:null,
+    selectedRowIndex:null,
+
+    showFooter: true,
+    footerHeight: 40 +'px',
+    reloadButtonHandler: null,
+    limitSelectorHandler:null,
+    limit:10,
+    offset: 0,
+    total: 100,
+    limitsList:[10, 25, 50],
+
+    headerHeight:40
 };
+
+
+
 
 
 ReactDOM.render( <SimpleDataTable
     data={fakeData}
+    reloadButtonHandler={()=>console.log("reload" )}
+    limitSelectorHandler={(limit)=>console.log("new limit:", limit)}
+    rowSelectHandler={(row, index)=>console.log("index rowSelectHandler", row, index)}
     columns={[
         {
             title: "First Name",
             width: 25,
-            key: "first_name"
+            key:   "first_name"
         }, {
             title: "Last Name",
             width: 25,
-            key: "last_name"
+            key:   "last_name"
         }, {
             title: "Gender",
             width: 25,
-            key: "gender"
+            key:   "gender"
         }, {
             title: "IP-address",
             width: 25,
-            key: "ip_address"
-        },{
+            key:   "ip_address"
+        }, {
             title: "email",
             width: 25,
-            key: "email"
+            key:   "email"
         }
     ]}
     bottomRow={false}
