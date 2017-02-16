6b5b9be3924113f06cb922a8b7e6c26101bab176
rusya13@gmail.com <rusya13@gmail.com>
2017-02-16 02:30:09 +0100

    start example to test API

--- Examples/test.js
+++ Examples/test.js
@@ -16,7 +16,7 @@ class TestApp extends React.Component{
                  name:   "First Name",
                  width:   25,
                  key:     "first_name",
-                 sortKey: "name"
+                 //sortKey: "name"
              }, {
                  name: "Last Name",
                  width: 25,
@@ -38,18 +38,17 @@ class TestApp extends React.Component{
 
         this.contextMenu = [
             { title: "Edit row", onClickHandler: () => console.log( "index action menu click" ) },
-
             { title: "Delete row", onClickHandler: () => console.log( "index action menu click" ) },
             { type:"divider" },
             { title: "Create new", onClickHandler: () => console.log( "index action menu click" ) },
-        ]
+        ];
 
-         this.state={selected : [0]};
+         this.state={selected : [0], offset:0, limit:10, orderBy:"first_name", dir:"ASC"};
     }
 
     sellClickHandler(row, index, column, buttons){
 
-        console.log("test sellClickHandler", index, column.key, buttons);
+        //console.log("test sellClickHandler", index, column.key, buttons);
         this.setState({selected:[index]})
     }
 
@@ -60,6 +59,17 @@ class TestApp extends React.Component{
 
 
     render(){
+        let min = this.state.offset;
+        let max = min + this.state.limit;
+        let field = this.state.orderBy;
+        let data = fakeData
+            .sort((a, b)=>{
+                if (this.state.dir === "ASC"){
+                    return a[field] < b[field]
+                } else {
+                    return a[field] > b[field]
+                }
+    }).filter((item, index)=>index < max && index > min)
 
 
         let footerButtons=[
@@ -69,14 +79,18 @@ class TestApp extends React.Component{
 
         return (
             <SimpleTable
-                data={fakeData}
+                data={data}
                 reloadButtonHandler={() => console.log( "reload" )}
-                limitSelectorHandler={( limit ) => console.log( "new limit:", limit )}
-                orderBy="name"
-                orderDirection="ASC"
+                limitSelectorHandler={( limit ) => this.setState({limit:Number(limit)})}
+                orderBy={this.state.orderBy}
+                orderDirection={this.state.dir}
+                limit={this.state.limit}
+                offset={this.state.offset}
                 orderChangeHandler={( key, order ) => {
-                    console.log( "index orderChangeHandler", key, order )
-
+                    if (this.state.orderBy !== key){
+                        this.state.orderBy = key
+                    }
+                    this.setState({dir:order, })
                 }}
                 columns={this.columns}
                 bottomRow={false}
